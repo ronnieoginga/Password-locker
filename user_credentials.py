19848f20bb9bfb5640e53197b8fa47fe50ea83fd
ronnieoginga <oginga001ronney@gmail.com>
2019-01-29 08:10:15 +0300

    added a function

--- user_credentials.py
+++ user_credentials.py
@@ -17,7 +17,7 @@ class User:
 		# instance variables
 		self.first_name = first_name
 		self.last_name = last_name
-		self.password = password.
+		self.password = password
 
 	def save_user(self):
 		'''
