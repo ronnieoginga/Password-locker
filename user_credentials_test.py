3fe4e1cba6a6e06147299edeb78ae7a22cd160b4
ronnieoginga <oginga001ronney@gmail.com>
2019-01-29 08:06:39 +0300

    updated test.py

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -26,26 +26,26 @@ class TestUser(unittest.TestCase):
 		Function to create a user account before each test
 		'''
 		self.new_user = User('ronnie','otieno','pswd100')
-#
-# 	def test__init__(self):
-# 		'''
-# 		Test to if check the initialization/creation of user instances is properly done
-# 		'''
-# 		self.assertEqual(self.new_user.first_name,'ronniei')
-# 		self.assertEqual(self.new_user.last_name,'otieno')
-# 		self.assertEqual(self.new_user.password,'pswd100')
-#
-# 	def test_save_user(self):
-# 		'''
-# 		Test to check if the new users info is saved into the users list
-# 		'''
-# 		self.new_user.save_user()
-# 		self.assertEqual(len(User.users_list),2)
-#
-# class TestCredentials(unittest.TestCase):
-# 	'''
-# 	Test class that defines test cases for the credentials class behaviours.
-# 	Args:
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of user instances is properly done
+		'''
+		self.assertEqual(self.new_user.first_name,'ronniei')
+		self.assertEqual(self.new_user.last_name,'otieno')
+		self.assertEqual(self.new_user.password,'pswd100')
+
+	def test_save_user(self):
+		'''
+		Test to check if the new users info is saved into the users list
+		'''
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),2)
+
+class TestCredentials(unittest.TestCase):
+	'''
+	Test class that defines test cases for the credentials class behaviours.
+	Args:
 	    unittest.TestCase: helps in creating test cases
 	'''
 
